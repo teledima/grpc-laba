971bb2c2e2ae4fce51cb9682da1076884d3d9b30
teledima <teledima00@gmail.com>
2021-12-29 11:47:03 +0500

    Initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,57 @@
+﻿#region snippet2
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Core;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        #region snippet
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            // Get all rows
+            var rows = new SourceGamesHelper("games.db").GetRows();
+
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://192.168.1.44:5001");
+            var client = new Exporter.ExporterClient(channel);
+
+            Console.WriteLine("Start export");
+            var callOptions = new CallOptions(deadline: DateTime.UtcNow.AddMinutes(2));
+            try
+            {
+                foreach (SourceGames sourceGame in rows)
+                {
+                    var reply = await client.ExportAsync(
+                                      new Row
+                                      {
+                                          GameName = sourceGame.GamesName,
+                                          AchievementName = sourceGame.AchievementsName,
+                                          CategoryName = sourceGame.CategoriesName,
+                                          DownloadableContentName = sourceGame.DownloadableContentsName
+                                      }, callOptions);
+                    Console.WriteLine(string.Format("Status export: {0}", reply.Ok));
+                }
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(string.Format("{0}, status code: {1}", ex.Status.Detail, ((uint)ex.StatusCode)));
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+        #endregion
+    }
+}
+#endregion
---
+++ GrpcClient/SourceGamesContext.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.Data.Sqlite;
+
+namespace GrpcGreeterClient
+{
+    public class SourceGamesHelper
+    {
+        public string Filepath { get; private set; }
+        public SourceGamesHelper(string filepath)
+        {
+            Filepath = filepath;
+        }
+
+        public List<SourceGames> GetRows()
+        {
+            var listResult = new List<SourceGames>();
+            using var connection = new SqliteConnection(string.Format("Data Source = {0}", Filepath));
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "select games_name, achievements_name, downloadable_contents_name, categories_name from source_games";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listResult.Add(new SourceGames()
+                {
+                    GamesName = reader.IsDBNull(0)?null: reader.GetString(0),
+                    AchievementsName = reader.IsDBNull(1) ? null : reader.GetString(1),
+                    DownloadableContentsName = reader.IsDBNull(2) ? null : reader.GetString(2),
+                    CategoriesName = reader.IsDBNull(3) ? null : reader.GetString(3)
+                });
+            }
+            return listResult;
+        }
+    }
+
+
+    public class SourceGames
+    {
+        public string GamesName { get; set; }
+
+        public string CategoriesName { get; set; }
+
+        public string DownloadableContentsName { get; set; }
+
+        public string AchievementsName { get; set; }
+    }
+}
